f022c0a529fa5a94ce98f33ebb2a5d1862ea85d5
Alihadjcherif <alim9@hotmail.fr>
2023-06-08 17:22:16 +0200

    fix:revert views

--- migrations/revert/views.sql
+++ migrations/revert/views.sql
@@ -1,14 +1,7 @@
--- Revert victoryzone:views from pg
-
-BEGIN;
 BEGIN;
 DROP VIEW IF EXISTS article_events_categories;
 DROP VIEW IF EXISTS player_view;
 
 -- XXX Add any other necessary revert operations here.
-
-COMMIT;
-
--- XXX Add DDLs here.
 
 COMMIT;
