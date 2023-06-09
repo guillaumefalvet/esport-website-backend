c7322bb467ad3295419227bb3c9fb33ff87f4bea
Guillaume Falvet <guillaume.falvet@outlook.com>
2023-06-09 14:35:24 +0200

    feat: added option to be in http or https mode

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+/* eslint-disable max-len */
 const debug = require('debug')('app:server');
 require('dotenv').config();
 
@@ -15,10 +16,29 @@ app.use(session({
   resave: false,
   saveUninitialized: false,
   cookie: {
-    secure: false, // This will only work if you have https enabled!
+    /**
+     *
+     * Determines the value of the 'secure' property based on the 'PROD' environment variable.
+     * The 'secure' property controls whether secure communication (e.g., HTTPS) is enabled or disabled.
+     * It is used to toggle certain settings or behaviors based on the environment.
+     *
+     * - If 'process.env.PROD' is truthy (e.g., has a non-empty string value), 'secure' will be set to 'true'.
+     * - If 'process.env.PROD' is falsy (e.g., undefined, null, empty string), 'secure' will be set to 'false'.
+     *
+     * Note: This code assumes the availability of the 'process.env.PROD' environment variable.
+     *
+     * Example usage:
+     *   - In a production environment, where 'PROD' is set to a truthy value:
+     *     secure: true
+     *
+     *   - In a development or testing environment, where 'PROD' is falsy or not defined:
+     *     secure: false
+     */
+    secure: !!process.env.PROD, // This will only work if you have https enabled!
     maxAge: 60000, // 1 min for debugging
   },
 }));
+debug(`Production mode ${!!process.env.PROD}`);
 app.use(express.json());
 app.use(router);
 
