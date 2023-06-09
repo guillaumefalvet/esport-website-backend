2af52cafa7d127b4fb35b908b0fb36b80afd164b
Guillaume Falvet <guillaume.falvet@outlook.com>
2023-06-09 08:59:39 +0200

    docs: added docs on mediarouter

--- app/routers/api/mediaRouter.js
+++ app/routers/api/mediaRouter.js
@@ -3,6 +3,22 @@ const controllerHandler = require('../../middlewares/controllerHandler');
 const { mediaController } = require('../../controllers');
 
 const router = express.Router();
+/**
+ * @typedef {object} Media
+ * @property {string} link - the media link
+ * @property {boolean} is_active - a boolean to differentiate a video from photo
+ * @property {string} created_at - date of creation
+ * @property {string} updated_at - date of last update
+ */
 
+/**
+ * GET /api/media?type=photo
+ * GET /api/media?type=video
+ * @summary get all medias
+ * @tags media
+ * @param {string} request.query.media
+ * @returns {Array<Media>} 200 - success
+ * @returns {object} 500 - internal server error
+ */
 router.get('/', controllerHandler(mediaController.getAll));
 module.exports = router;
