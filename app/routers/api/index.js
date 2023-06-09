d967625936912cfa94273ff15baaf21adb49406b
Guillaume Falvet <guillaume.falvet@outlook.com>
2023-06-09 12:56:52 +0200

    fix: for the time being, commented the unfished route

--- app/routers/api/index.js
+++ app/routers/api/index.js
@@ -8,11 +8,11 @@ const authRouter = require('./authRouter');
 
 const router = express.Router();
 
-router.use('/media', mediaRouter);
-router.use('/articles', articleRouter);
-router.use('/team', teamRouter);
-router.use('/calendrier', calendarRouter);
-router.use('/recruitment', recruitmentRouter);
+// router.use('/media', mediaRouter);
+// router.use('/articles', articleRouter);
+// router.use('/team', teamRouter);
+// router.use('/calendrier', calendarRouter);
+// router.use('/recruitment', recruitmentRouter);
 router.use('/auth', authRouter);
 /**
  * error handlers for 404 here
