498caf92027d80c6686f319c961c361f97bf5b32
Guillaume Falvet <guillaume.falvet@outlook.com>
2023-06-09 13:02:02 +0200

    config: adding session on server

--- index.js
+++ index.js
@@ -2,11 +2,22 @@ const debug = require('debug')('app:server');
 require('dotenv').config();
 
 const express = require('express');
+const session = require('express-session');
 const router = require('./app/routers');
 
 const port = process.env.PORT || 3000;
 
 const app = express();
+app.use(session({
+  name: process.env.SESSION_NAME,
+  secret: process.env.SESSION_SECRET,
+  resave: false,
+  saveUninitialized: false,
+  cookie: {
+    secure: false, // This will only work if you have https enabled!
+    maxAge: 60000, // 1 min for debugging
+  },
+}));
 app.use(express.json());
 app.use(router);
 
