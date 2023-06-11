27e94f879000d067f4190e965b67c013d44f4458
Guillaume Falvet <guillaume.falvet@outlook.com>
2023-06-11 12:12:46 +0200

    fix: removed some condition on article schemas

--- app/validations/schemas/article-schema.js
+++ app/validations/schemas/article-schema.js
@@ -5,23 +5,23 @@ const createArticle = Joi.object({
   title: Joi.string().required(),
   content: Joi.string().required(),
   author: Joi.string().required(),
-  small_image: Joi.string().uri().required(),
-  medium_image: Joi.string().uri().required(),
-  large_image: Joi.string().uri().required(),
+  small_image: Joi.string().required(),
+  medium_image: Joi.string().required(),
+  large_image: Joi.string().required(),
   publication_date: Joi.string().required(),
   figcaption: Joi.string(),
-}).require();
+}).required();
 
 const modifyArticle = Joi.object({
   slug: Joi.string(),
   title: Joi.string(),
   content: Joi.string(),
   author: Joi.string(),
-  small_image: Joi.string().uri(),
-  medium_image: Joi.string().uri(),
-  large_image: Joi.string().uri(),
+  small_image: Joi.string(),
+  medium_image: Joi.string(),
+  large_image: Joi.string(),
   publication_date: Joi.string(),
   figcaption: Joi.string(),
-}).require().min(1);
+}).required().min(1);
 
 module.exports = { createArticle, modifyArticle };
