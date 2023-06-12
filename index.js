a04feb5028e02ced44c720c006bfb73295135613
Alihadjcherif <alim9@hotmail.fr>
2023-06-12 13:25:19 +0200

    add cors modul

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const debug = require('debug')('app:server');
 require('dotenv').config();
 const express = require('express');
 const expressJSDocSwagger = require('express-jsdoc-swagger');
+const cors = require('cors');
 const router = require('./app/routers');
 
 const port = process.env.PORT || 3000;
@@ -27,11 +28,18 @@ const options = {
   // Expose OpenAPI UI
   exposeSwaggerUI: true,
 };
+const corsOptions = {
+  origine: '*', // Autoriser toutes les origines (vous pouvez spécifier des origines spécifiques si nécessaire)
+  methode: ['GET', 'POST', 'PUT', 'DELETE'], // Autoriser les méthodes HTTP spécifiées
+  allowedHeader: ['Content-Type', 'Authorization'], // Autoriser les en-têtes spécifiés
+
+};
 app.use(express.urlencoded({ extended: true }));
 
 debug(`Production mode ${!!process.env.PROD}`);
 expressJSDocSwagger(app)(options);
 app.use(express.json());
+app.use(cors(corsOptions));
 app.use(router);
 
 app.listen(port, () => {
