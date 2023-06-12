4fc11489c6a8ac81b94d47e111b880a2f81fc04d
Guillaume Falvet <guillaume.falvet@outlook.com>
2023-06-12 16:11:39 +0200

    docs: added jsdoc for auth route

--- app/routers/api/authRouter.js
+++ app/routers/api/authRouter.js
@@ -2,27 +2,46 @@ const express = require('express');
 const controllerHandler = require('../../middlewares/controllerHandler');
 const { loginValidation } = require('../../validations/schemas/login-schema');
 const validate = require('../../validations/validate');
-const { authController } = require('../../controllers');
+const authController = require('../../controllers/authController');
 
 const router = express.Router();
+
 /**
- * @typedef {object} Login
- * @property {string} email - login email
- * @property {string} password - login password
+ * @typedef {object} LoginRequestBody
+ * @property {string} email - The email for login.
+ * @property {string} password - The password for login.
  */
 
 /**
  * POST /api/auth/login
  *
- * @summary login for a non authentified user
- * @tags Login
- * @param {Login} request.body
- * @returns {object} 200 - successful login
- * @returns {object} 401 - invalid credentials
+ * @summary Handles user login.
+ * @tags Authentication
+ *
+ * @requestBody {LoginRequestBody} request.body - The login request body.
+ *
+ * @returns {object} 200 - Successful login.
+ * @returns {object} 400 - Invalid credentials (wrong email or password).
+ * @returns {object} 500 - Internal server error.
  */
-
 router.post('/login', validate(loginValidation), controllerHandler(authController.login));
 
+/**
+ * POST /api/auth/refresh-token
+ *
+ * @summary Refreshes the access token using a refresh token.
+ * @tags Authentication
+ *
+ * @requestBody {object} Request body
+ * @requestBodyExample {object} Example request body
+ *     {
+ *       "refreshToken": "your_refresh_token_here"
+ *     }
+ *
+ * @returns {object} 200 - Success response with new access and refresh tokens.
+ * @returns {object} 401 - Unauthorized access.
+ * @returns {object} 500 - Internal server error.
+ */
 router.post('/refresh-token', controllerHandler(authController.tokenRefresh));
 
 module.exports = router;
