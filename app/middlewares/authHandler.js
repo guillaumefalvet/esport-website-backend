e700be8b95fa27f2203ae63481c3cc2e48655240
Guillaume Falvet <guillaume.falvet@outlook.com>
2023-06-13 22:13:19 +0200

    fix: removed sensible informations of the jwt token

--- app/middlewares/authHandler.js
+++ app/middlewares/authHandler.js
@@ -5,29 +5,16 @@ const dataMapper = require('../models/dataMapper');
 const { JWT_SECRET, JWT_REFRESH_SECRET } = process.env;
 const ACCESS_TOKEN_EXPIRATION = process.env.ACCESS_TOKEN_EXPIRATION ?? '15m';
 const REFRESH_TOKEN_EXPIRATION = process.env.REFRESH_TOKEN_EXPIRATION ?? '7d';
-/**
- * Middleware functions related to authentication and authorization.
- * @module authHandler
- */
 
 module.exports = {
-  /**
-   * Generates an access token.
-   *
-   * @param {string} ip - The IP address of the user.
-   * @param {Object} user - The user object containing user information.
-   * @returns {string} The generated access token.
-   */
-  generateAccessTokenWithUser(ip, user) {
-    debug(`generateAccessTokenWithUser, ip:${ip},user: ${user}`);
+
+  generateAccessTokenWithUser(user) {
+    debug(`generateAccessTokenWithUser: ${user}`);
     return jwt.sign(
       {
         data: {
-          ip,
           id: user.id,
-          email: user.email,
           user_name: user.user_name,
-          permission_name: user.permission_name,
           permission_level: user.permission_level,
         },
       },
@@ -36,12 +23,6 @@ module.exports = {
     );
   },
 
-  /**
-   * Generates a refresh token.
-   *
-   * @param {string} id - The user ID.
-   * @returns {string} The generated refresh token.
-   */
   generateRefreshTokenForUser(id) {
     debug(`generateRefreshTokenForUser for id :${id}`);
     debug(jwt.sign({ id }, JWT_REFRESH_SECRET, {
@@ -52,12 +33,6 @@ module.exports = {
     });
   },
 
-  /**
-   * Middleware function to authorize access based on permission level.
-   *
-   * @param {number} permissionLevelRequired - The required permission level.
-   * @returns {Function} The authorization middleware function.
-   */
   authorizeAccess(permissionLevelRequired) {
     return async (request, _, next) => {
       debug(`authorize middleware for level: ${permissionLevelRequired}`);
@@ -72,11 +47,6 @@ module.exports = {
         }
         const token = authHeader.split('Bearer ')[1];
         const decoded = jwt.verify(token, JWT_SECRET);
-        if (decoded.data.ip !== request.ip) {
-          const error = new Error();
-          error.code = 401;
-          return next(error);
-        }
         if (decoded.data.permission_level !== permissionLevelRequired) {
           const error = new Error();
           error.code = 403;
@@ -92,12 +62,6 @@ module.exports = {
     };
   },
 
-  /**
-   * Checks if a refresh token is valid.
-   *
-   * @param {string} token - The refresh token to validate.
-   * @returns {Promise<boolean>} A promise resolving to `true` if the token is valid, or `false` otherwise.
-   */
   async isRefreshTokenValid(token) {
     const decodedToken = jwt.verify(token, JWT_REFRESH_SECRET);
     const storedToken = await dataMapper.getRefreshToken(decodedToken.id);
@@ -105,19 +69,13 @@ module.exports = {
     return token === storedToken;
   },
 
-  /**
-   * Retrieves the user associated with a token.
-   *
-   * @param {string} token - The token containing user information.
-   * @returns {Promise<Object>} A promise resolving to the user object.
-   */
   async getUserFromToken(token) {
     const decoded = jwt.verify(
       token,
       JWT_SECRET,
       { ignoreExpiration: true },
     );
-    debug(`getUserFromToken: ${decoded.data.email}`);
-    return dataMapper.getByEmail(decoded.data.email);
+    debug(`getUserFromToken: ${decoded.data.id}`);
+    return dataMapper.getByPk('user', decoded.data.id);
   },
 };
