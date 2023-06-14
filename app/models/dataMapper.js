27219da767144d6e93827d678e976a3328644d44
Guillaume Falvet <guillaume.falvet@outlook.com>
2023-06-14 17:38:31 +0200

    feat: added the necessary method for the the new routes in team (/media /setup)

--- app/models/dataMapper.js
+++ app/models/dataMapper.js
@@ -29,6 +29,14 @@ module.exports = {
     const { rows } = await client.query(`SELECT * FROM insert_${table}($1);`, [JSON.stringify(data)]);
     return rows[0];
   },
+  async getReferenceTable(parent, child, parentValue, childValue) {
+    const { rows } = await client.query(`SELECT * FROM ${parent}_has_${child} WHERE ${parent}_id = $1 AND ${child}_id = $2;`, [parentValue, childValue]);
+    return rows[0];
+  },
+  async insertReferenceTable(parent, child, parentValue, childValue) {
+    const { rows } = await client.query(`INSERT INTO ${parent}_has_${child}("${parent}_id", "${child}_id") VALUES ($1, $2) RETURNING *;`, [parentValue, childValue]);
+    return rows[0];
+  },
   async modifyByPk(table, data) {
     const { rows } = await client.query(`SELECT * FROM update_${table}($1);`, [JSON.stringify(data)]);
     return rows[0];
