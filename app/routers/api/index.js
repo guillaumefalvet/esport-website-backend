547451f097c760829fa734ff204b8aa5028486c9
Guillaume Falvet <guillaume.falvet@outlook.com>
2023-06-14 17:48:51 +0200

    feat: uncommenting the team route, making it available to everyone

--- app/routers/api/index.js
+++ app/routers/api/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const mediaRouter = require('./mediaRouter');
 const articleRouter = require('./articleRouter');
-// const teamRouter = require('./teamRouter');
+const teamRouter = require('./teamRouter');
 // const calendarRouter = require('./calendarRouter');
 const recruitmentRouter = require('./recruitmentRouter');
 const { error404, errorHandler } = require('../../middlewares/errorHandler');
@@ -11,7 +11,7 @@ const router = express.Router();
 
 router.use('/media', mediaRouter);
 router.use('/articles', articleRouter);
-// router.use('/team', teamRouter);
+router.use('/team', teamRouter);
 // router.use('/calendrier', calendarRouter);
 router.use('/recruitment', recruitmentRouter);
 router.use('/auth', authRouter);
