b24f11cb1a979b4edb513ae516a3d339587dd37e
Alihadjcherif <alim9@hotmail.fr>
2023-06-15 14:13:16 +0200

    feat: create/update/delet on mediaControler

--- app/models/dataMapper.js
+++ app/models/dataMapper.js
@@ -17,6 +17,10 @@ module.exports = {
     const { rows } = await client.query(`SELECT * FROM "${table}" WHERE id = $1;`, [id]);
     return rows;
   },
+  async getByLink(table, link) {
+    const { rows } = await client.query(`SELECT * FROM ${table} WHERE link = $1`, [link]);
+    return rows;
+  },
   async getBySlug(slug) {
     const result = await client.query('SELECT * FROM article_events_categories WHERE slug = $1;', [slug]);
     return result;
@@ -42,6 +46,10 @@ module.exports = {
     return rows[0];
   },
   async modifyByUserName(table, data) {
+    const { rows } = await client.query(`SELECT * FROM update_${table}($1);`, [JSON.stringify(data)]);
+    return rows[0];
+  },
+  async modifyByLink(table, data) {
     const { rows } = await client.query(`SELECT * FROM update_${table}($1);`, [JSON.stringify(data)]);
     return rows[0];
   },
