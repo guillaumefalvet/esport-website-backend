40d95b555e2a26d47eb699193609d01786cb34cf
Guillaume Falvet <guillaume.falvet@outlook.com>
2023-06-16 21:19:59 +0200

    fix: added case where user wouldn't provide a file

--- app/middlewares/uploadHandler.js
+++ app/middlewares/uploadHandler.js
@@ -4,7 +4,7 @@ const multer = require('multer');
 /**
  * Handles file upload using Multer middleware.
  *
- * @param {Object} req - The request object.
+ * @param {Object} request - The request object.
  * @param {string} mainFolder - The main folder where files will be uploaded.
  * @param {string} subFolder - The subfolder within the main folder where files will be uploaded.
  * @param {string} fieldname - The name of the field in the request containing the file.
@@ -12,7 +12,7 @@ const multer = require('multer');
  * @returns {Promise<Object>} A Promise that resolves to an object containing the uploaded file information.
  * @throws {Error} If there is an error during file upload.
  */
-const uploadHandler = (req, mainFolder, subFolder, fieldname, next) => {
+const uploadHandler = (request, mainFolder, subFolder, fieldname, next) => {
   const uploadFolder = `${mainFolder}/${subFolder}`;
 
   const storage = multer.diskStorage({
@@ -29,16 +29,20 @@ const uploadHandler = (req, mainFolder, subFolder, fieldname, next) => {
   const upload = multer({ storage });
 
   return new Promise((resolve, reject) => {
-    upload.single(fieldname)(req, null, (err) => {
+    upload.single(fieldname)(request, null, (err) => {
       if (err) {
         // Pass the error to the next middleware
         return reject(err);
       }
+      // If there isn't any file => send empty string
+      if (!request.file) {
+        return resolve({ filename: '', path: 'no file provided' });
+      }
       // File upload successful
-      const { filename, path } = req.file;
+      const { filename, path } = request.file;
 
       // Process the file and resolve the Promise with the result
-      resolve({ filename, path });
+      return resolve({ filename, path });
     });
   })
     .then((result) => result)
