625e8993f83f2dfa487b7e2b7870e03218c246fa
Guillaume Falvet <guillaume.falvet@outlook.com>
2023-06-16 16:02:08 +0200

    fix: removed some code that wasnt suppose to be there

--- app/controllers/recruitmentController.js
+++ app/controllers/recruitmentController.js
@@ -2,27 +2,7 @@
 const debug = require('debug')('app:controllers:recruitController');
 const uploadHandler = require('../middlewares/uploadHandler');
 const dataMapper = require('../models/dataMapper');
-/**
- * a recruitment type
- * @typedef {object} Recruitment
- * @property {number} id - a recruitment id
- * @property {string} user_name - recruitment user_name
- * @property {string} email - recruitment email
- * @property {string} first_name - recruitment first name
- * @property {string} last_name - recruitment last name
- * @property {string} message - recruitment message
- * @property {string} external_link - recruitment external link
- * @property {string} created_at - date of creation
- * @property {string} updated_at - date of last update
- */
 
-/**
- * POST /api/recruitment
- * @summary insert recruitment
- * @tags Recruitment
- * @returns {object} 200 - success message
- * @returns {object} 400 - bad request
- */
 module.exports = {
   async insertOne(request, response, next) {
     const image_upload = await uploadHandler(request, 'private', 'pdf', 'cv', next);
