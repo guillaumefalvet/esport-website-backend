15e1f324cfff98905aaf2043d17b945db77677cb
Guillaume Falvet <guillaume.falvet@outlook.com>
2023-06-16 17:12:15 +0200

    feat: added figcaption for homepage small view

--- migrations/deploy/views.sql
+++ migrations/deploy/views.sql
@@ -117,7 +117,7 @@ ORDER BY player.id;
 
 
 CREATE VIEW get_article_home AS
-SELECT "article"."id", "article"."slug", "article"."title", "article"."image", "article"."content" FROM "article" WHERE "article"."publication_date" <= now() limit 3;
+SELECT "article"."id", "article"."slug", "article"."title", "article"."image", "article"."figcaption", "article"."content" FROM "article" WHERE "article"."publication_date" <= now() limit 3;
 
 CREATE VIEW get_user_view AS
 SELECT "user"."id", "user"."user_name", "user"."email", "user"."password", "user"."refresh_token", "permission"."name" AS permission_name, "permission"."level" AS permission_level ,"user"."created_at", "user"."updated_at" FROM "user" JOIN "permission" ON "permission"."id" = "user"."user_permission";
