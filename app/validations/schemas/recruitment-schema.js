a002fa4212a7e00985836bdf9fe8c76f987ae3a5
Guillaume Falvet <guillaume.falvet@outlook.com>
2023-06-17 15:07:08 +0200

    feat: added an allow on external link, in case the user doesn't provide a file

--- app/validations/schemas/recruitment-schema.js
+++ app/validations/schemas/recruitment-schema.js
@@ -6,7 +6,7 @@ const createRecruitment = Joi.object({
   first_name: Joi.string().required(),
   last_name: Joi.string().required(),
   message: Joi.string().required(),
-  external_link: Joi.string(),
-}).required().min(5);
+  external_link: Joi.string().allow(''),
+}).required();
 
 module.exports = { createRecruitment };
