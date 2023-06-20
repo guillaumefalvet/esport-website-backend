2c83d0651b35911f9e8de4ee725d777350ffa488
Guillaume Falvet <guillaume.falvet@outlook.com>
2023-06-20 16:36:28 +0200

    refactor: extended the coreController to media

--- app/controllers/mediaController.js
+++ app/controllers/mediaController.js
@@ -1,43 +1,51 @@
-/* eslint-disable camelcase */
-const debug = require('debug')('app:controllers:mediaController');
+/* eslint-disable max-len */
+const debug = require('debug')('app:controllers:media');
 const fs = require('fs');
 const dataMapper = require('../models/dataMapper');
 const uploadService = require('../services/uploadService');
 const { createMedia } = require('../validations/schemas/media-schema');
+const CoreController = require('./CoreController');
 
 const API_URL = process.env.API_URL ?? '';
-const tableName = 'media';
+
 const jsend = {
-  status: '',
-  data: '',
+  status: 'success',
 };
-module.exports = {
-  async getAll(request, response) {
-    debug(`getAll ${tableName}`);
-    debug(request.query);
-    // check the type of the query, false for photo.
+
+class MediaController extends CoreController {
+  static tableName = 'media';
+
+  static columnName = 'id';
+
+  constructor() {
+    super();
+    debug('mediaController created');
+  }
+
+  async getAllMedia(request, response) {
+    debug(`${this.constructor.name} get all`);
     if (request.query.type === 'photo') {
+      debug('photo');
       const result = await dataMapper.getByType(false);
       jsend.status = 'success';
       jsend.data = result;
       return response.status(200).json(jsend);
     }
-    // check the type of the query, true for video.
     if (request.query.type === 'video') {
+      debug('video');
       const result = await dataMapper.getByType(true);
       jsend.data = result;
       return response.status(200).json(jsend);
     }
-    //  if no query type
-    const results = await dataMapper.getAll(tableName);
+    const results = await dataMapper.getAll(this.constructor.tableName);
     jsend.data = results;
 
     return response.status(200).json(jsend);
-  },
+  }
 
-  async insertOne(request, response, next) {
+  async insertMedia(request, response, next) {
     try {
-      debug(`insertOne ${tableName}`);
+      debug(`${this.constructor.name} insertMedia`);
       const imageUpload = await uploadService(request, 'public', 'image', 'img', next);
       const updatedData = {};
       if (!imageUpload.path.length) {
@@ -50,32 +58,31 @@ module.exports = {
         updatedData.is_active = false;
       }
       await createMedia.validateAsync(updatedData);
-      const result = await dataMapper.createOne(tableName, updatedData);
-      jsend.status = 'success';
+      const result = await dataMapper.createOne(this.constructor.tableName, updatedData);
       jsend.data = result;
       return response.status(201).json(jsend);
     } catch (error) {
       debug('error while uploading');
       return next(error);
     }
-  },
+  }
 
-  async deleteOne(request, response, next) {
-    debug(` deleteOne ${tableName}`);
-    const { id } = request.params;
-    const findMedia = await dataMapper.getByPk(tableName, id);
-    if (!findMedia.length) {
+  async deleteMedia(request, response, next) {
+    debug(`${this.constructor.name} deleteMedia`);
+    const findMedia = await dataMapper.getByColumnValue(this.constructor.tableName, this.constructor.columnName, request.params[this.constructor.columnName]);
+    if (!findMedia) {
       const error = new Error();
       error.code = 404;
       return next(error);
     }
-    if (!findMedia[0].is_active) { // if = false
+    if (!findMedia.is_active) { // if = false
       // delete locally
-      const removed_url = findMedia[0].link.split(API_URL);
-      fs.unlinkSync(`./${removed_url[1]}`);
+      const localFile = findMedia.link.split(API_URL);
+      fs.unlinkSync(`./${localFile[1]}`);
       debug('FS: media deleted ');
     }
-    await dataMapper.deleteByPk(tableName, id);
+    await dataMapper.deleteByColumnValue(this.constructor.tableName, this.constructor.columnName, request.params[this.constructor.columnName]);
     return response.status(204);
-  },
-};
+  }
+}
+module.exports = new MediaController();
