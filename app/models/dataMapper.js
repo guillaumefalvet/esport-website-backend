48aad5df508b2f7b3ea1fbed42af0ee1dffe64aa
Guillaume Falvet <guillaume.falvet@outlook.com>
2023-06-20 16:55:20 +0200

    remove: removed unused method from the datamapper

--- app/models/dataMapper.js
+++ app/models/dataMapper.js
@@ -1,41 +1,17 @@
 const client = require('./clients');
 
 module.exports = {
-  async getUser(userName) {
-    const { rows } = await client.query('SELECT * FROM "user" WHERE user_name = $1', [userName]);
-    return rows;
-  },
   async getByColumnValue(tableName, columnName, value) {
     const { rows } = await client.query(`SELECT * FROM ${tableName} WHERE ${columnName} = $1`, [value]);
     return rows[0];
   },
-  async getByType(boolean) {
-    const { rows } = await client.query('SELECT * FROM "media" WHERE is_active = $1', [boolean]);
-    return rows;
-  },
   async getAll(table) {
-    const { rows } = await client.query(`SELECT * FROM ${table} ORDER BY "id" DESC; `);
-    return rows;
-  },
-  async getByPk(table, id) {
-    const { rows } = await client.query(`SELECT * FROM "${table}" WHERE id = $1;`, [id]);
-    return rows;
-  },
-  async getByLink(table, link) {
-    const { rows } = await client.query(`SELECT * FROM ${table} WHERE link = $1`, [link]);
-    return rows;
-  },
-  async getBySlug(slug) {
-    const { rows } = await client.query('SELECT * FROM article_events_categories_public WHERE slug = $1;', [slug]);
-    return rows;
-  },
-  async getByUserName(table, userName) {
-    const { rows } = await client.query(`SELECT * FROM ${table} WHERE user_name = $1;`, [userName]);
+    const { rows } = await client.query(`SELECT * FROM ${table}`);
     return rows;
   },
   async getByEmail(email) {
     const { rows } = await client.query('SELECT * FROM get_user_view WHERE email = $1;', [email]);
-    return rows;
+    return rows[0];
   },
   async createOne(table, data) {
     const { rows } = await client.query(`SELECT * FROM insert_${table}($1);`, [JSON.stringify(data)]);
@@ -49,37 +25,12 @@ module.exports = {
     const { rows } = await client.query(`INSERT INTO ${parent}_has_${child}("${parent}_id", "${child}_id") VALUES ($1, $2) RETURNING *;`, [parentValue, childValue]);
     return rows[0];
   },
-  async modifyByPk(table, data) {
-    const { rows } = await client.query(`SELECT * FROM update_${table}($1);`, [JSON.stringify(data)]);
-    return rows[0];
-  },
   async modifyOne(table, data) {
     const { rows } = await client.query(`SELECT * FROM update_${table}($1);`, [JSON.stringify(data)]);
     return rows[0];
   },
-  async modifyBySlug(data) {
-    const { rows } = await client.query('SELECT * FROM update_article($1);', [JSON.stringify(data)]);
-    return rows[0];
-  },
-  async modifyByUserName(table, data) {
-    const { rows } = await client.query(`SELECT * FROM update_${table}($1);`, [JSON.stringify(data)]);
-    return rows[0];
-  },
-  async modifyByLink(table, data) {
-    const { rows } = await client.query(`SELECT * FROM update_${table}($1);`, [JSON.stringify(data)]);
-    return rows[0];
-  },
-  async deleteByPk(table, id) {
-    await client.query(`DELETE FROM ${table} WHERE id = $1;`, [id]);
-  },
   async deleteByColumnValue(tableName, columnName, value) {
     await client.query(`DELETE FROM ${tableName} WHERE ${columnName} = $1;`, [value]);
-  },
-  async deleteBySlug(slug) {
-    await client.query('DELETE FROM article WHERE slug = $1;', [slug]);
-  },
-  async deleteByUserName(table, userName) {
-    await client.query(`DELETE FROM ${table} WHERE user_name = $1;`, [userName]);
   },
   async getRefreshToken(id) {
     const preparedQuery = {
@@ -96,13 +47,5 @@ module.exports = {
     };
     const { rows } = await client.query(preparedQuery);
     return rows;
-  },
-  async getCalendarHome() {
-    const { rows } = await client.query('SELECT * FROM get_calendar_home');
-    return rows[0];
-  },
-  async getAllCalendar() {
-    const result = await client.query('SELECT * FROM get_all_calendar');
-    return result.rows[0];
   },
 };
