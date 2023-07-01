b9958013c21fcede050a5a1eda938c26871e7b5f
Guillaume Falvet <guillaume.falvet@outlook.com>
2023-07-01 15:39:58 +0200

    🔧 config: jest config & script

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,10 +1,11 @@
 module.exports = {
   env: {
-
     commonjs: true,
     es2021: true,
     node: true,
+    'jest/globals': true,
   },
+  plugins: ['jest'],
   extends: 'airbnb-base',
   overrides: [
   ],
@@ -12,5 +13,10 @@ module.exports = {
     ecmaVersion: 'latest',
   },
   rules: {
+    'jest/no-disabled-tests': 'warn',
+    'jest/no-focused-tests': 'error',
+    'jest/no-identical-title': 'error',
+    'jest/prefer-to-have-length': 'warn',
+    'jest/valid-expect': 'error',
   },
 };
