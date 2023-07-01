c7a6230ea28510f772445988675e92f6bd26bed9
Guillaume Falvet <guillaume.falvet@outlook.com>
2023-07-01 15:56:45 +0200

    ✨ feat: added 404, and error handler on the root endpoint

--- app/routers/index.js
+++ app/routers/index.js
@@ -2,18 +2,16 @@ const express = require('express');
 const serveIndex = require('serve-index');
 const apiRouter = require('./api');
 const { authorizeAccess } = require('../middlewares/authHandler');
+const { error404, errorHandler } = require('../middlewares/errorHandler');
 
 const router = express.Router();
 
-/**
- * every request starting with /api/.. will be handled here
- */
 router.use('/api', apiRouter);
 router.use('/public', express.static('public'));
 router.use('/public', serveIndex('public', { icons: true }));
 router.use('/private', authorizeAccess(1), express.static('private'));
 
-/**
- * error handlers for 404 here
- */
+router.use(error404);
+router.use(errorHandler);
+
 module.exports = router;
