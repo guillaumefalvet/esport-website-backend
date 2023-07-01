d45816396112d2f3483d344bd6d718acb46e7f8a
Guillaume Falvet <guillaume.falvet@outlook.com>
2023-07-01 22:04:42 +0200

    ✨ feat: added node-cron, and set it up to run every sunday at 9:30 paris time

--- index.js
+++ index.js
@@ -1,13 +1,31 @@
 const debug = require('debug')('app:server');
 require('dotenv').config();
+const cron = require('node-cron');
 const express = require('express');
 const cors = require('cors');
 const router = require('./app/routers');
 const { corsOptions } = require('./app/services/corsService');
 const swaggerService = require('./app/services/swaggerService');
+const backupService = require('./app/services/backupService');
 
-const port = process.env.PORT || 3000;
 const app = express();
+const port = process.env.PORT || 3000;
+/**
+ * Executes a scheduled backup task using cron.
+ *
+ * @param {string} cronExpression - The cron expression specifying the schedule of the task.
+ * for more info about the syntax https://www.gnu.org/software/mcron/manual/html_node/Crontab-file.html
+ * @param {Function} task - The task to be executed on the scheduled interval.
+ * @param {Object} options - Additional options for the cron scheduler.
+ * @param {string} options.timezone - The timezone in which the task should run.
+ */
+// EVERY SUNDAY AT 9:30 (Paris time), it will fun the function
+cron.schedule('30 9 * * SUN', () => {
+  debug('sunday backup');
+  backupService.dump();
+}, {
+  timezone: 'Europe/Paris',
+});
 swaggerService(app, `${__dirname}/app/routers`);
 app.use(express.urlencoded({ extended: true }));
 debug(`Production mode ${!!process.env.PROD}`);
