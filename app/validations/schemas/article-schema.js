d43a8c6bce63c4ee4a98b3fee7c22ddf21dd597f
Guillaume Falvet <guillaume.falvet@outlook.com>
2023-07-09 00:52:57 +0200

    ✨ feat: added extra validation for article title

--- app/validations/schemas/article-schema.js
+++ app/validations/schemas/article-schema.js
@@ -1,17 +1,18 @@
 const Joi = require('joi');
 
 const createArticle = Joi.object({
-  title: Joi.string().required(),
+  title: Joi.string().regex(/^[a-zA-Z0-9' ]+$/).required(),
   content: Joi.string().required(),
   publication_date: Joi.string().required(),
   figcaption: Joi.string().allow(null, ''),
 }).required();
 
 const modifyArticle = Joi.object({
-  title: Joi.string(),
+  title: Joi.string().regex(/^[a-zA-Z0-9' ]+$/),
   content: Joi.string(),
   publication_date: Joi.string(),
   figcaption: Joi.string(),
 }).required().min(1);
 
+// .regex(/^[a-zA-Z0-9']+$/)
 module.exports = { createArticle, modifyArticle };
