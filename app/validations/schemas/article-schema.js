75f33777555d7aad22111b52e8716c92b5ac5733
Guillaume Falvet <guillaume.falvet@outlook.com>
2023-07-10 16:22:18 +0200

    ♻️ refactor: less strict validation on title

--- app/validations/schemas/article-schema.js
+++ app/validations/schemas/article-schema.js
@@ -1,14 +1,14 @@
 const Joi = require('joi');
 
 const createArticle = Joi.object({
-  title: Joi.string().regex(/^[a-zA-Z0-9' ]+$/).required(),
+  title: Joi.string().regex(/^[a-zA-Z0-9\s'-?!@&:/]+$/).required(),
   content: Joi.string().required(),
   publication_date: Joi.string().required(),
   figcaption: Joi.string().allow(null, ''),
 }).required();
 
 const modifyArticle = Joi.object({
-  title: Joi.string().regex(/^[a-zA-Z0-9' ]+$/),
+  title: Joi.string().regex(/^[a-zA-Z0-9\s'-?!@&:/]+$/),
   content: Joi.string(),
   publication_date: Joi.string(),
   figcaption: Joi.string(),
