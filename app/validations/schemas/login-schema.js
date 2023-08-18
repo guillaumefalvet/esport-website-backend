27ff780f97503a2dcfb844c2530414163dae3d43
Guillaume Falvet <guillaume.falvet@outlook.com>
2023-08-18 17:40:41 +0200

    :lock: security: password minimum length increased

--- app/validations/schemas/login-schema.js
+++ app/validations/schemas/login-schema.js
@@ -2,7 +2,7 @@ const Joi = require('joi');
 
 const loginValidation = Joi.object({
   email: Joi.string().required().email(),
-  password: Joi.string().required().min(3).max(20),
+  password: Joi.string().required().min(8).max(20),
 }).required();
 
 const refreshToken = Joi.object({
